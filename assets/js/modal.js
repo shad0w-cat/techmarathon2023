894f1e8600811d7b4b39f3a584e7998372c79951
HY <62005923+shad0w-cat@users.noreply.github.com>
2023-01-31 13:15:40 +0530

    pages.css path corrected

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
             xhr.open('GET', `./pages/${this.alt}.html`, true);
             xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4 && xhr.status === 200) {
-                    modalDiv.style.visibility = "unset";
+                    modalDiv.style.visibility = "visible";
                     var dom = new DOMParser().parseFromString(xhr.responseText, 'text/html')
                     document.getElementById("modalBody").innerHTML = xhr.responseText;
                     document.getElementById("modalHeader").innerHTML = `<span id="closeModalBtn" onclick="closeModal()">&times;</span>` + dom.getElementById("root").getAttribute("data-event");
